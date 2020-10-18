a3534447dcf695f01a9885b78c52b4764a9bc9ae
KN-Kevinoh <lekankevinoh@gmail.com>
2020-10-18 19:07:34 +0700

    update project

--- exam_monitoring/settings.py
+++ exam_monitoring/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u^13f8o)wzpc@pgp3k4lq9*e=v9%yhta*m+t5qg-xtis=7_l7'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
